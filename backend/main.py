afea3b451b67713134f7cf22b6d6814b3056433e
ComicalTrader <guilhermeaires70@gmail.com>
2025-08-21 12:02:12 -0300

    Implemented uvicorn and routers

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from .database import Base,engine
+from .routers import appointment
+Base.metadata.create_all(bind= engine)
 app = FastAPI("title= management API")
 
 
-app.get("/health")
+app.get("/h")
 def health(): return{"status": "good"}
 
---
+++ backend/routers/appointment.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from ..database import SessionLocal
+from ..models.appointment import Appointment
+from ..schemas.appointment import AppointmentCreate,AppointmentOut
+
+
+router = APIRouter(Prefix = "/appointments", tags = ["appointments"])
+def get_db():
+    db = SessionLocal()
+    try: yield db
+    finally: db.close()
+
+
+
+@router.post("",response_model=AppointmentOut)
+def create(ap:AppointmentCreate, db: Session = Depends(get_db)):
+    obj = Appointment(**ap.model_dump)
+    db.add(obj); db.commit(); db.refresh(obj); return obj
+
+
+
+@router.get("",response_model= list [AppointmentOut])
+def list_all(db: Session = Depends(get_db)):
+    return db.query(Appointment).all()
+
